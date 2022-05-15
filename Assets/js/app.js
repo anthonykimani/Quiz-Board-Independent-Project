2e22fdb12af3bf933974a11fdc3e249901ee962e
anthonykimani <anthonykimani@student.moringaschool.com>
2022-05-15 12:48:31 +0300

    adds querySelectors to radio elements

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -10,3 +10,16 @@ const answerChoice9 = document.querySelector('.nine');
 const answerChoice10 = document.querySelector('.ten');
 const answerChoice11 = document.querySelector('.eleven');
 const answerChoice12 = document.querySelector('.twelve');
+
+const radioChoice1 = document.querySelector('.radio-one');
+const radioChoice2 = document.querySelector('.radio-two');
+const radioChoice3 = document.querySelector('.radio-three');
+const radioChoice4 = document.querySelector('.radio-four');
+const radioChoice5 = document.querySelector('.radio-five');
+const radioChoice6 = document.querySelector('.radio-six');
+const radioChoice7 = document.querySelector('.radio-seven');
+const radioChoice8 = document.querySelector('.radio-eight');
+const radioChoice9 = document.querySelector('.radio-nine');
+const radioChoice10 = document.querySelector('.radio-ten');
+const radioChoice11 = document.querySelector('.radio-eleven');
+const radioChoice12 = document.querySelector('.radio-twelve');
