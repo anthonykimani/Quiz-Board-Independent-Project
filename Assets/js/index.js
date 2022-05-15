3eaf6db12cdb421bacad57beff59fbb633aeb8b2
anthonykimani <anthonykimani@student.moringaschool.com>
2022-05-15 12:42:44 +0300

    Updates the index.js to add querySelectors for DOM manipulation

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,6 +1,10 @@
-const startButton = document.querySelector('.submit');
 const formCard = document.querySelector('.form-container');
 const quizCard = document.querySelector('.quiz-container');
+const scoreCard = document.querySelector('.scoreboard-container');
+const submitButton = document.querySelector('.button');
+const startButton = document.querySelector('.startbutton');
+let totalScore = document.querySelector('.score');
+let startQuestion , sortQuestion ;
 
 
 
@@ -11,3 +15,4 @@ function startQuiz(){
     formCard.classList.add('hide');
     quizCard.classList.remove('hide');
 }
+
