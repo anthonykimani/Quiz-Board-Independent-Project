64121db016836be897dba8f77b38b93959894364
anthonykimani <anthonykimani@student.moringaschool.com>
2022-05-23 15:46:41 +0300

    restructures the js file to make use of arrays

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,185 +1,46 @@
-const answerChoice1 = document.querySelector('.one');
-const answerChoice2 = document.querySelector('.two');
-const answerChoice3 = document.querySelector('.three');
-const answerChoice4 = document.querySelector('.four');
-const answerChoice5 = document.querySelector('.five');
-const answerChoice6 = document.querySelector('.six');
-const answerChoice7 = document.querySelector('.seven');
-const answerChoice8 = document.querySelector('.eight');
-const answerChoice9 = document.querySelector('.nine');
-const answerChoice10 = document.querySelector('.ten');
-const answerChoice11 = document.querySelector('.eleven');
-const answerChoice12 = document.querySelector('.twelve');
+questions = [
+  {
+    question: "WHAT DOES HTML STAND FOR",
+    answer: "HYPERTEXT MARKUP LANGUAGE",
+    choices: [
+      "HELLO PROGRAMMING LANGUAGE",
+      "HYPERTEXT MARKUP LANGUAGE",
+      "HAPPY TERMINAL LEARNING",
+      "HUNDRED LINES OF CODE",
+    ],
+  },
+  {
+    question: "WHAT DOES CSS STAND FOR",
+    answer: "CASCADING STYLESHEET",
+    choices: [
+      "CASCADING STYLESHEET",
+      "CARS STYLING STRUCTURE",
+      "CONDUSIVE SITE STRUCTURING",
+      "SCANDID SPLENDING HONEY",
+    ],
+  },
+];
 
-const radioChoice1 = document.querySelector('.radio-one');
-const radioChoice2 = document.querySelector('.radio-two');
-const radioChoice3 = document.querySelector('.radio-three');
-const radioChoice4 = document.querySelector('.radio-four');
-const radioChoice5 = document.querySelector('.radio-five');
-const radioChoice6 = document.querySelector('.radio-six');
-const radioChoice7 = document.querySelector('.radio-seven');
-const radioChoice8 = document.querySelector('.radio-eight');
-const radioChoice9 = document.querySelector('.radio-nine');
-const radioChoice10 = document.querySelector('.radio-ten');
-const radioChoice11 = document.querySelector('.radio-eleven');
-const radioChoice12 = document.querySelector('.radio-twelve');
+const quizTitle = document.querySelector(".quiz-title");
+const quizAnswer = document.querySelector(".quiz-answers");
+const submitButton = document.querySelector(".button");
 
-let correct = 0;
+let i = 0;
 
-answerChoice1.addEventListener('click', ()=>{
-    radioChoice1.checked = true;
-    answerChoice1.style.backgroundColor = "#d3e2fa";
-    radioChoice2.checked = false;
-    answerChoice2.style.backgroundColor = "#fff";
-    radioChoice3.checked = false;
-    answerChoice3.style.backgroundColor = "#fff";
-    radioChoice4.checked = false;
-    answerChoice4.style.backgroundColor = "#fff";
-    correct++;
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-answerChoice2.addEventListener('click', ()=>{
-    radioChoice1.checked = false;
-    answerChoice1.style.backgroundColor = "#fff";
-    radioChoice2.checked = true;
-    answerChoice2.style.backgroundColor = "#d3e2fa";
-    radioChoice3.checked = false;
-    answerChoice3.style.backgroundColor = "#fff";
-    radioChoice4.checked = false;
-    answerChoice4.style.backgroundColor = "#fff";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
+  let questionTag = `${questions[i].question}`;
+  let answerTag =
+    '<span class="quiz-answer"> ' +
+    questions[i].choices[0] +
+    "</span>" +
+    '<span class="quiz-answer"> ' +
+    questions[i].choices[1] +
+    "</span>" +
+    '<span class="quiz-answer"> ' +
+    questions[i].choices[2] +
+    "</span>" +
+    '<span class="quiz-answer"> ' +
+    questions[i].choices[3] +
+    "</span>";
 
-answerChoice3.addEventListener('click', ()=>{
-    radioChoice1.checked = false;
-    answerChoice1.style.backgroundColor = "#fff";
-    radioChoice2.checked = false;
-    answerChoice2.style.backgroundColor = "#fff";
-    radioChoice3.checked = true;
-    answerChoice3.style.backgroundColor = "#d3e2fa";
-    radioChoice4.checked = false;
-    answerChoice4.style.backgroundColor = "#fff";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice4.addEventListener('click', ()=>{
-    radioChoice1.checked = false;
-    answerChoice1.style.backgroundColor = "#fff";
-    radioChoice2.checked = false;
-    answerChoice2.style.backgroundColor = "#fff";
-    radioChoice3.checked = false;
-    answerChoice3.style.backgroundColor = "#fff";
-    radioChoice4.checked = true;
-    answerChoice4.style.backgroundColor = "#d3e2fa";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice5.addEventListener('click', ()=>{
-    radioChoice5.checked = true;
-    answerChoice5.style.backgroundColor = "#d3e2fa";
-    radioChoice6.checked = false;
-    answerChoice6.style.backgroundColor = "#fff";
-    radioChoice7.checked = false;
-    answerChoice7.style.backgroundColor = "#fff";
-    radioChoice8.checked = false;
-    answerChoice8.style.backgroundColor = "#fff";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice6.addEventListener('click', ()=>{
-    radioChoice5.checked = false;
-    answerChoice5.style.backgroundColor = "#fff";
-    radioChoice6.checked = true;
-    answerChoice6.style.backgroundColor = "#d3e2fa";
-    radioChoice7.checked = false;
-    answerChoice7.style.backgroundColor = "#fff";
-    radioChoice8.checked = false;
-    answerChoice8.style.backgroundColor = "#fff";
-    correct+=2;
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice7.addEventListener('click', ()=>{
-    radioChoice5.checked = false;
-    answerChoice5.style.backgroundColor = "#fff";
-    radioChoice6.checked = false;
-    answerChoice6.style.backgroundColor = "#fff";
-    radioChoice7.checked = true;
-    answerChoice7.style.backgroundColor = "#d3e2fa";
-    radioChoice8.checked = false;
-    answerChoice8.style.backgroundColor = "#fff";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice8.addEventListener('click', ()=>{
-    radioChoice5.checked = false;
-    answerChoice5.style.backgroundColor = "#fff";
-    radioChoice6.checked = false;
-    answerChoice6.style.backgroundColor = "#fff";
-    radioChoice7.checked = false;
-    answerChoice7.style.backgroundColor = "#fff";
-    radioChoice8.checked = true;
-    answerChoice8.style.backgroundColor = "#d3e2fa";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice9.addEventListener('click', ()=>{
-    radioChoice9.checked = true;
-    answerChoice9.style.backgroundColor = "#d3e2fa";
-    radioChoice10.checked = false;
-    answerChoice10.style.backgroundColor = "#fff";
-    radioChoice11.checked = false;
-    answerChoice11.style.backgroundColor = "#fff";
-    radioChoice12.checked = false;
-    answerChoice12.style.backgroundColor = "#fff";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice10.addEventListener('click', ()=>{
-    radioChoice9.checked = false;
-    answerChoice9.style.backgroundColor = "#fff";
-    radioChoice10.checked = true;
-    answerChoice10.style.backgroundColor = "#d3e2fa";
-    radioChoice11.checked = false;
-    answerChoice11.style.backgroundColor = "#fff";
-    radioChoice12.checked = false;
-    answerChoice12.style.backgroundColor = "#fff";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice11.addEventListener('click', ()=>{
-    radioChoice9.checked = false;
-    answerChoice9.style.backgroundColor = "#fff";
-    radioChoice10.checked = false;
-    answerChoice10.style.backgroundColor = "#fff";
-    radioChoice11.checked = true;
-    answerChoice11.style.backgroundColor = "#d3e2fa";
-    radioChoice12.checked = false;
-    answerChoice12.style.backgroundColor = "#fff";
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
-
-answerChoice12.addEventListener('click', ()=>{
-    radioChoice9.checked = false;
-    answerChoice9.style.backgroundColor = "#fff";
-    radioChoice10.checked = false;
-    answerChoice10.style.backgroundColor = "#fff";
-    radioChoice11.checked = false;
-    answerChoice11.style.backgroundColor = "#fff";
-    radioChoice12.checked = true;
-    answerChoice12.style.backgroundColor = "#d3e2fa";
-    correct+=2;
-    console.log(correct);
-    totalScore.innerText = correct*20;
-});
+  quizTitle.textContent = questionTag;
+  quizAnswer.innerHTML = answerTag;
--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,23 +1,26 @@
 const formCard = document.querySelector('.form-container');
 const quizCard = document.querySelector('.quiz-container');
 const scoreCard = document.querySelector('.scoreboard-container');
-const submitButton = document.querySelector('.button');
 const startButton = document.querySelector('.startbutton');
 let totalScore = document.querySelector('.score');
 let startQuestion , sortQuestion ;
 
-
-
-
-startButton.addEventListener('click', startQuiz);
-
-function startQuiz(){
+const startQuiz = ()=>{
     formCard.classList.add('hide');
     quizCard.classList.remove('hide');
 }
 
-submitButton.addEventListener('click',function(){
+const showResult = ()=>{
     scoreCard.classList.remove('hide');
     quizCard.classList.add('hide');
-})
+}
+
+startButton.addEventListener('click', startQuiz);
+
+
+
+
+
+
+
 
